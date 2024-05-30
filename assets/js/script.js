922a261254a020be8a6566021627804f3a3186cc
Roberto Perez <robertom.perez@outlook.com>
2024-05-29 21:22:43 -0300

    intro JS

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+
+
+
+// nombre = 'Roberto'
+// edad = prompt('Que edad tiene?')
+// alert( 'La  edad de ' + nombre + ' es ' + edad)
+
+
+
+// num1 = 2
+
+// num2 = 2
+
+// resultado = num1 + num2
+
+// console.log(resultado)
+
+// elemento = document.querySelector('p')
+// elemento.innerHTML = 'HOLA ALUMNOS!'
+// elemento.style.color = 'red'
+// elemento.style.fontSize = '30px'
+// elemento.style.fontWeight = 'bold'
+
+imagePerfil = document.querySelector('img')
+radiusImg = prompt('Ingrese el valor del redius')
+imagePerfil.style.borderRadius = radiusImg
